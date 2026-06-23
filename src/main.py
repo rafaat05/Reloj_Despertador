24512667a9ad2352e9d7f2150eeaa80339ea10d1
Rafael Arias <rafa.at3013@gmail.com>
2026-06-23 19:21:03 +0200

    feat(Sript-de-main-actualizado): main.py implementado de manera funcional con prevencion de errores en longitud de lista

--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
-#miplementacion de todas las funcionalidades del proyecto en ub codigo limpio
-#Primero la creacion de una lista donde añadir las url
-#2 configuracion de la hroa de la alarma por el usuario
-#3 ejecucion de la alarma con la url seleccionada aleatoriamente
-
-print("Reloj despertador.")
-# En main.py
 from utils import gestion_url, configuracion_alarma, ejecutar_alarma
 
-# creaciond de lista
-lista= gestion_url()
+if __name__ == "__main__":
+    print("Reloj despertador.")
 
-#configuracion de alarma.
-seleccion=configuracion_alarma()
+    # creacion de lista
+    lista= gestion_url()
 
-#ejecucion
-output=ejecutar_alarma(lista)
+    #configuracion de alarma si lista > 0
+    if lista>0:
+
+        seleccion=configuracion_alarma()
+    else:
+        print("Lista de seleccion de url vacia.")
+        quit
+
+    #ejecucion
+    output=ejecutar_alarma(lista)
